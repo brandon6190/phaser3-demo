b6d6508e059eabcf861df4d1afee2e9f32e7d2cd
brandon6190 <brandon6190@outlook.com>
2019-06-18 23:58:15 -0400

    Inital node express server build

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { join } = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(join(__dirname, '../assets')));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '../view/index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server listening on port:', port)
+})
