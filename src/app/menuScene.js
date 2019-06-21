5c83a077568d8436081701645720185b0cc5a511
brandon6190 <brandon6190@outlook.com>
2019-06-21 01:04:01 -0400

    assigned menuScene with key of "menu" whithin create();   add the sky image   add particles   create emitter for particles   add the logo image to the games physics
      set the logos velocity, bounce, and collider
      set emmiter to follow logo

---
+++ src/app/menuScene.js
@@ -0,0 +1,24 @@
+export default {
+
+  key: 'menu',
+
+  create () {
+    this.add.image(400, 300, 'sky');
+
+    const particles = this.add.particles('red');
+
+    const emitter = particles.createEmitter({
+      speed: 50,
+      scale: { start: 0.5, end: 0 },
+      blendMode: 'ADD'
+    });
+
+    const logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+  }
+};
