7e4d91d7c17a1179fc881677efb9bc155f9f2292
brandon6190 <brandon6190@outlook.com>
2019-06-22 00:58:04 -0400

    removed canvas element; removed canvas property and changed the type property to Phaser.AUTO; set debug prop in the arcade obj to false

--- src/app/gameConfig.js
+++ src/app/gameConfig.js
@@ -1,24 +1,18 @@
 import Phaser from 'phaser';
 import bootScene from './bootScene';
 import menuScene from './menuScene';
-
-const gameCanvas = document.createElement('canvas');
-
-gameCanvas.id = 'game-world';
-gameCanvas.style = 'border: 1px solid firebrick';
-
-document.body.appendChild(gameCanvas);
+import playScene from './playScene';
 
 export default {
-  type: Phaser.CANVAS,
+  type: Phaser.AUTO,
   width: 800,
-  height: 620,
-  canvas: document.getElementById('game-world'),
+  height: 600,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      gravity: { y: 200 },
+      debug: false
     }
   },
-  scene: [bootScene, menuScene]
+  scene: [bootScene, menuScene, playScene]
 };
