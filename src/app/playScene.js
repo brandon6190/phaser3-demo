86f6a6433049ab32c4aae25a22820d9b122ec154
brandon6190 <brandon6190@outlook.com>
2019-06-25 01:38:32 -0400

    added 11 stars to game world. iterating over all the stars to set a random bounce

--- src/app/playScene.js
+++ src/app/playScene.js
@@ -1,7 +1,8 @@
-// import Phaser from 'phaser';
+import Phaser from 'phaser';
 let platforms;
 let player;
 let cursors;
+let stars;
 
 export default {
   key: 'play',
@@ -25,6 +26,16 @@ export default {
     this.physics.add.collider(player, platforms);
 
     cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
   },
 
   update () {
